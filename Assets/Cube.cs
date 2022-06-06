fe6ceec53d3aeef0646044ca40ea7d7f8868db2f
TheL1ght <77700198+TheL1ght@users.noreply.github.com>
2022-06-06 12:05:19 -0300

    Made Magic Cube fall and added a floor so it can fall on

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -7,18 +7,46 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int numberOfTimes = 2;
-        string usedKey = "ESC";
-        float seconds = 999.99f;
+        // int numberOfTimes = 2;
+        // string usedKey = "ESC";
+        // float seconds = 999.99f;
 
-        Debug.Log("Press left arrow key" + numberOfTimes + " to go left.");
-        Debug.LogWarning("WARNING: Pressing " + usedKey + " key will stop the program.");
-        Debug.LogError("ERROR: You took " + seconds + " seconds to press left arrow.");
+        // Debug.Log("Press left arrow key" + numberOfTimes + " to go left.");
+        // Debug.LogWarning("WARNING: Pressing " + usedKey + " key will stop the program.");
+        // Debug.LogError("ERROR: You took " + seconds + " seconds to press left arrow.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // message if down arrow is pressed.
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up Arrow was pressed.");
+        }
+
+        // message if down arrow is pressed.
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down Arrow was pressed.");
+        }
+
+        // message if left arrow is pressed.
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("Left Arrow was pressed.");
+        }
+
+        // message if right arrow is pressed.
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            print("Right Arrow was pressed.");
+        }
+
+        // message if space bar is pressed.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Space Bar was pressed.");
+        }
     }
 }
