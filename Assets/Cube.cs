1999c4f176a30887b77b80c762e14b19a1c37a3f
TheL1ght <77700198+TheL1ght@users.noreply.github.com>
2022-06-06 13:33:58 -0300

    Makes Magic Cube able to go up, down, left and right.

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    public Rigidbody2D MyRigidbody2D;
+
+    // int numberOfTimes = 2;
+    // string usedKey = "ESC";
+    // float seconds = 999.99f;
     // Start is called before the first frame update
+
     void Start()
     {
-        // int numberOfTimes = 2;
-        // string usedKey = "ESC";
-        // float seconds = 999.99f;
-
         // Debug.Log("Press left arrow key" + numberOfTimes + " to go left.");
         // Debug.LogWarning("WARNING: Pressing " + usedKey + " key will stop the program.");
         // Debug.LogError("ERROR: You took " + seconds + " seconds to press left arrow.");
@@ -19,34 +21,34 @@ public class Cube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // message if down arrow is pressed.
+        // message if up arrow is pressed.
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            print("Up Arrow was pressed.");
+            MyRigidbody2D.velocity = new Vector2(0f, 10f);
         }
 
         // message if down arrow is pressed.
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            print("Down Arrow was pressed.");
-        }
-
-        // message if left arrow is pressed.
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            print("Left Arrow was pressed.");
+            MyRigidbody2D.velocity = new Vector2(0f, -10f);
         }
 
         // message if right arrow is pressed.
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            print("Right Arrow was pressed.");
+            MyRigidbody2D.velocity = new Vector2(10f, 0f);
+        }
+
+        // message if left arrow is pressed.
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MyRigidbody2D.velocity = new Vector2(-10f, 0f);
         }
 
         // message if space bar is pressed.
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            print("Space Bar was pressed.");
+            MyRigidbody2D.velocity = new Vector2(0f, 10f);
         }
     }
 }
